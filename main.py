e449eeb54004390318ce36758a4db40dfdb6297a
Lazare Gogoladze <lazaregogoladzedev@gmail.com>
2024-05-03 15:03:39 +0400

    Added new buttons for menu

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from Register_user import Register_user
 from Variablies import EX_INPUT_BALANCE, EX_BASE_ERROR
 from balance import add_balance, transaction
+from user import get_user_info
+from loan_calculator import calculate_loan
+
 def main():      
     while True:
         Operation = input(''' ::::::Operations::::::: 
         1.Register_User
         2.Add_Balance
         3.Transfer_money
-        4.Exit
+        4.Show account details  
+        5.Show transaction history
+        6.Loan calculator                                  
+        7.Exit
         Choose Operation:: ''')
         try: 
             if Operation == '1':
@@ -17,7 +23,13 @@ def main():
                 add_balance()
             if Operation == '3':
                 transaction()
-            elif Operation == '4':
+            if Operation == '4':
+                iban = input("Enter user IBAN to show account details: ")
+                get_user_info(iban)
+            if Operation == '6':
+                iban = input("Enter user IBAN to calculate loan :")
+                calculate_loan(iban)
+            elif Operation == '7':
                 break
         except:
             print(EX_BASE_ERROR) 
