f9572c9514350c9aa1bba7b0b34293be9cf364c7
dolchadoli <nikolozkhidasheli10@gmail.com>
2024-05-03 21:52:28 +0400

    historys naxvis principi shevcvale

--- history.py
+++ history.py
@@ -17,3 +17,17 @@ def print_balance_history(user_id):
                 print(balance_event)
             return
     print("User not found.")
+
+def view_history():
+    history_type = input(''' Which history would you like to see?
+        1. Balance History
+        2. Transaction History
+        Choose Operation:: ''')
+    if history_type == '1':
+        user_id = input("Enter your ID: ")
+        print_balance_history(user_id)
+    elif history_type == '2':
+        user_id = input("Enter your ID: ")
+        print_transaction_history(user_id)
+    else:
+        print("Invalid choice! Please enter either '1' or '2'.")
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Register_user import Register_user
 from Variablies import EX_INPUT_BALANCE, EX_BASE_ERROR
 from balance import add_balance, transaction
-from history import print_balance_history, print_transaction_history
+from history import print_balance_history, print_transaction_history, view_history
 
 def main():      
     while True:
@@ -9,7 +9,8 @@ def main():
         1.Register_User
         2.Add_Balance
         3.Transfer_money
-        4.Exit
+        4.View_History
+        5.Exit
         Choose Operation:: ''')
         try: 
             if Operation == '1':
@@ -17,13 +18,11 @@ def main():
                 print(f"successfully created {k['name']} {k['surname']}'s account {k['id']} and funded {k['balance']} lari")
             if Operation == '2':
                 add_balance()
-                user_id = input("Enter your ID: ")
-                print_balance_history(user_id)
             if Operation == '3':
                 transaction()
-                user_id = input("Enter your ID: ")
-                print_transaction_history(user_id)
             elif Operation == '4':
+                view_history()
+            elif Operation == '5':
                 break
         except:
             print(EX_BASE_ERROR) 
