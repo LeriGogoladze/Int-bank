370604a9e3716276108726f5ec213db659caf0fc
mikheil godoladze <mikheil.godoladze@yahoo.com>
2024-05-20 23:16:24 +0400

    added new feature

--- Register_user.py
+++ Register_user.py
@@ -1,16 +1,23 @@
 from Variablies import DB, EX_INPUT_BALANCE, MAX_BALANCE_AMOUNT, EX_INPUT_NUMBER
 from Validation import user_number_gen, user_check_base
+import csv
 
 def Register_user():
-    try:
-        user = [input('Enter user name: '), input('Enter last name: ')]
-        balance = int(input('Enter amount: '))
-        if balance > MAX_BALANCE_AMOUNT:
-            raise Exception(EX_INPUT_BALANCE)
-        if  user_check_base(user_number_gen()):    
-            DB.append({"id": user_number_gen(),"name": user[0], "surname": user[1], "balance": balance, "loan": 8.2})
-            return DB[len(DB)-1]    
-    except None:
-        print(EX_INPUT_BALANCE)
-    except ValueError:
-        print(EX_INPUT_NUMBER)    
+    
+    user = [input('Enter user name: '), input('Enter last name: ')]
+    balance = int(input('Enter amount: '))
+    if balance > MAX_BALANCE_AMOUNT:
+        raise Exception(EX_INPUT_BALANCE)
+    if  user_check_base(user_number_gen()):    
+        DB.append({"id": user_number_gen(),"name": user[0], "surname": user[1], "balance": balance, "loan": 8.2})
+        with open('USERS.csv', 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames = DB[0].keys(), quoting=csv.QUOTE_NONNUMERIC, delimiter=' ',)
+            writer.writeheader()
+            writer.writerow(DB[len(DB)-1])
+            file.close
+        return DB[len(DB)-1]
+
+#except None:
+#    print(EX_INPUT_BALANCE)
+#except ValueError:
+#    print(EX_INPUT_NUMBER)    
--- Variablies.py
+++ Variablies.py
@@ -1,4 +1,4 @@
-DB = [{"id": "TB0001", "name": "davit", "surname": "darjania", "balance": 20, "loan": 0}]
+DB = [{"id": "TB0001", "name": "davit", "surname": "darjania", "balance": 20, "loan": 8.2}]
 TB = [{"sender": "TB0001", "reciver": "TB0002", "amount": 50}]
 EX_INPUT_BALANCE = "Balance amount must be below 100, try again"
 EX_INPUT_NUMBER = "Entered information is not number"
